a1ba45e85f340328cdbbbd1b7bacd781832efb12
Yuriy Kravets <97830497+yuriykravets@users.noreply.github.com>
2023-04-29 20:45:04 +0300

    Initial commit

---
+++ app/src/main/java/com/partitionsoft/geoquiz/MainActivity.kt
@@ -0,0 +1,108 @@
+package com.partitionsoft.geoquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+import com.partitionsoft.geoquiz.databinding.ActivityMainBinding
+
+private const val TAG = "MainActivity"
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private var currentIndex = 0
+    private var numCorrectAnswers = 0
+    private var numQuestionsAnswered = 0
+
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true)
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle?) called")
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.questionTextView.setOnClickListener { view: View ->
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+
+        binding.trueButton.setOnClickListener { view: View ->
+            checkAnswer(true)
+        }
+
+        binding.falseButton.setOnClickListener { view: View ->
+            checkAnswer(false)
+        }
+
+        binding.nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+        binding.previousButton.setOnClickListener {
+            currentIndex = (currentIndex - 1) % questionBank.size
+            updateQuestion()
+        }
+        updateQuestion()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
+    }
+
+    private fun updateQuestion() {
+        val questionTextResId = questionBank[currentIndex].textResId
+        binding.questionTextView.setText(questionTextResId)
+    }
+
+    private fun checkAnswer(userAnswer: Boolean) {
+        val correctAnswer = questionBank[currentIndex].answer
+        numQuestionsAnswered++
+
+        if (userAnswer == correctAnswer) {
+            numCorrectAnswers++
+            Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show()
+        }
+
+        if (numQuestionsAnswered == questionBank.size) {
+            val percentageScore = (numCorrectAnswers.toDouble() / numQuestionsAnswered.toDouble()) * 100
+            Toast.makeText(this, "Your score is $percentageScore%", Toast.LENGTH_LONG).show()
+        }
+    }
+
+}
---
+++ app/src/main/java/com/partitionsoft/geoquiz/Question.kt
@@ -0,0 +1,9 @@
+package com.partitionsoft.geoquiz
+
+import androidx.annotation.StringRes
+
+data class Question(
+    @StringRes
+    val textResId: Int,
+    val answer: Boolean
+)
